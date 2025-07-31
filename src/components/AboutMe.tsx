7cdb25d22260db8e9c0e6a860248aa23640a6dc5
dominickdevincenzo <devindm101@gmail.com>
2025-07-31 18:09:49 -0400

    fixed ESLint build error

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,14 +4,14 @@ function AboutMe() {
       {/* Bio Section */}
       <div className="space-y-4">
         <p>
-          I'm an aspiring software engineer currently working as a Data
+          I&apos;m an aspiring software engineer currently working as a Data
           Coordinator on oncology clinical trials at Georgetown University
-          Medical Center. I'm also gearing up to start my final semester before
-          completing my Master's in Computer and Information Technology at the
-          University of Pennsylvania.
+          Medical Center. I&apos;m also gearing up to start my final semester
+          before completing my Master&apos;s in Computer and Information
+          Technology at the University of Pennsylvania.
         </p>
         <p>
-          Through the program, I've gained hands-on experience with core
+          Through the program, I&apos;ve gained hands-on experience with core
           computer science topics including software engineering, computer
           systems, databases, and networked applications. In my final semester,
           I’ll be taking Machine Learning for Data Science and Internet and Web
@@ -19,9 +19,10 @@ function AboutMe() {
         </p>
         <p>
           My background blends data, research, and software - driven by a strong
-          interest in building tools that solve real-world problems. When I'm
-          not coding or studying, you’ll find me tinkering with my espresso
-          machine, spending time with my family, or petting every dog I meet.
+          interest in building tools that solve real-world problems. When
+          I&apos;m not coding or studying, you’ll find me tinkering with my
+          espresso machine, spending time with my family, or petting every dog I
+          meet.
         </p>
       </div>
 
